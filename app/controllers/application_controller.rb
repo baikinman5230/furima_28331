85bc08dca426e8127de760337f2c961cd064d643
baikinman5230 <koudai.0818@icloud.com>
2020-08-25 19:33:44 +0900

    余分箇所の削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,9 @@
 class ApplicationController < ActionController::Base
-  #before_action :basic_auth
-  #before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday])
-  end
-
-  private
-
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == 'koudai' && password == '0000'
-    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -36,9 +36,6 @@ RSpec.describe User, type: :model do
         another_user.valid?
         expect(another_user.errors.full_messages).to include("Email has already been taken")
       end
-      it "emailに@が含まれてなければ登録できない" do
-        
-      end
       it "passwordが空では登録できない" do
         @user.password = ""
         @user.valid?
