38296cf417c0e0eaf3620fcb34c123fee7f7dfe7
baikinman5230 <koudai.0818@icloud.com>
2020-08-31 20:13:59 +0900

    自動計算機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,17 +8,18 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new(item_params)
+    @item = Item.create(item_params)
     if @item.save
       redirect_to root_path
     else
       render :new
     end
+    
   end
 
   private
 
   def item_params
-    params.repuire(:item).permit(:image,:product,:discription,:price,:category_id,:product_condition_id,:shipping_fee_burde_id,:shipping_region_id,:days_until_shipping_id)
+    params.permit(:image,:product,:discription,:price,:category_id,:product_condition_id,:shipping_fee_burde_id,:shipping_region_id,:days_until_shipping_id)
   end
 end
