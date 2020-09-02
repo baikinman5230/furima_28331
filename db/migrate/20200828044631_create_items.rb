24f171d3a0ccf24158a69257a4e88063653eef84
baikinman5230 <koudai.0818@icloud.com>
2020-09-02 16:13:38 +0900

    sold out の表示に対する条件分岐

--- db/migrate/20200828044631_create_items.rb
+++ db/migrate/20200828044631_create_items.rb
@@ -9,6 +9,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.integer    :shipping_fee_burde_id,  null: false
       t.integer    :shipping_region_id,     null: false
       t.integer    :days_until_shipping_id, null: false
+      t.integer    :buyer_id
       t.references :user,                   null: false, foreign_key: true
       t.timestamps
     end
