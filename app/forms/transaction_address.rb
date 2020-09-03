887691baeaf30b3b1f00da1ac5896706ebd5ba7f
baikinman5230 <koudai.0818@icloud.com>
2020-09-04 06:57:14 +0900

    単体テストコード

--- app/forms/transaction_address.rb
+++ app/forms/transaction_address.rb
@@ -4,10 +4,10 @@ class TransactionAddress
   attr_accessor :postal_code, :prefecture, :city, :house_number, :building_name, :phone_number, :item_id, :token, :user_id
 
   with_options presence: true do
-    validates :postal_code
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
     validates :city
     validates :house_number
-    validates :phone_number
+    validates :phone_number, format: {with: /\A\d{11}\z/}
   end
 
   with_options numericality: {other_than: 1} do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_03_160954) do
+ActiveRecord::Schema.define(version: 2020_09_03_161322) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -33,25 +33,13 @@ ActiveRecord::Schema.define(version: 2020_09_03_160954) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "addreses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "postal_cord", null: false
-    t.integer "prefecture_id", null: false
+  create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "postal_code", null: false
+    t.integer "prefecture", null: false
     t.string "city", null: false
     t.string "house_number", null: false
     t.string "building_name"
     t.string "phone_number", null: false
-    t.bigint "item_purchase_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["item_purchase_id"], name: "index_addreses_on_item_purchase_id"
-  end
-
-  create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "postal_code", default: "", null: false
-    t.integer "prefecture", null: false
-    t.string "city", default: ""
-    t.string "house_number", default: ""
-    t.string "building_name", default: ""
     t.bigint "item_purchase_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -101,7 +89,6 @@ ActiveRecord::Schema.define(version: 2020_09_03_160954) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "addreses", "item_purchases"
   add_foreign_key "addresses", "item_purchases"
   add_foreign_key "item_purchases", "items"
   add_foreign_key "item_purchases", "users"
